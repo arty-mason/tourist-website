3566eaec32f0fe219ce4f89e3a2d5ce3666e25d3
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-12-12 14:00:11 +0300

    Creating a mobile adaptive navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import NavBar from './components/NavBar';
 import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <NavBar />
     </div>
   );
 }
