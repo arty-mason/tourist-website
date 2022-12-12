f1b7ebee8977a0db85b8fad1360170f718319a22
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-12-12 14:47:15 +0300

    Background layout for the Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-[90vh]">
+      <img
+        src="https://images.unsplash.com/photo-1494477920939-88c59ba76428?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80"
+        alt=""
+        className="w-full h-full object-cover"
+      />
+      <div className="max-w-[1140px] m-auto">
+        <div className="absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-white p-4">
+          <h1 className="font-bold text-4xl">Find Your Special Trip</h1>
+          <h2 className="text-4xl py-4 italic">With Weekaway</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis
+            pariatur, natus voluptate dolor exercitationem repellat, modi sed,
+            ex incidunt quia architecto neque porro tempore placeat. Temporibus
+            in itaque fuga consectetur.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
