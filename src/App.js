85d69980807f90874403b1709d39648b8696937e
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-12-12 16:23:00 +0300

    Adding & styling footer to the app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Activities from './components/Activities';
 import Booking from './components/Booking';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Gallery from './components/Gallery';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
@@ -10,7 +11,7 @@ import TopBar from './components/TopBar';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <TopBar />
       <NavBar />
       <Hero />
@@ -18,7 +19,8 @@ function App() {
       <Booking />
       <Gallery />
       <Contact />
-    </div>
+      <Footer />
+    </>
   );
 }
 
